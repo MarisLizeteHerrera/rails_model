863a8b22bcdb1471cae3d6e222d1a64ef97d6482
Maris Herrera <maris.lizete.herrera@gmail.com>
2018-11-13 22:02:29 -0700

    finish project

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,7 @@
 class PeopleController < ApplicationController
   # CRUD - Create Read Update Destroy
 
-  # index and show are "READ"
+
   def index
     @people = Person.all
   end
@@ -10,7 +10,6 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
   end
 
-  # # new is for create
   def new
     @person = Person.new
   end
@@ -24,18 +23,29 @@ class PeopleController < ApplicationController
     end
   end
 
-  def destroy
-    @person = Person.find(params[:id]).destroy
+  def edit
+    @person = Person.find(params[:id])
   end
 
-  # # update is for updating
-  # def update
-  #   @person = Person.find(params[:id]).
-  # end
+  def update
+    @person = Person.find(params[:id])
+  
+    if @person.update(person_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_path
+  end
+
  private
 
   def person_params
-    params.require(:person).permit(:name, :age, :hair_color, :eye_color)
+    params.require(:person).permit(:name, :age, :hair_color, :eye_color, :gender)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+
   resources :people
 
 end
+
